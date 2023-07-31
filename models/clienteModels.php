b2902e678dbce22854ef3b8ff644722d10e1e1d2
Romel16 <romel_faustino@hotmail.com>
2023-07-31 16:02:40 -0500

    31/07/2023

---
+++ models/clienteModels.php
@@ -0,0 +1,61 @@
+<?php
+
+    class ClienteModels extends Conectar{
+        /*TODO: Listar Cliente*/
+        public function getCliente_x_sucursalId($idempresa) {
+            $conectar = parent::Conexion();
+            $sql = "spListarCliente ?";
+            $query = $conectar->prepare($sql);
+            $query->bindValue(1,$idempresa);
+            $query->execute();
+            return $query->fetchAll(PDO::FETCH_ASSOC);
+        }
+        /*TODO: Listar Cliente por Id*/
+        public function getCliente_x_id($idcliente){
+            $conectar = parent::Conexion();
+            $sql = "spListarClienteporId ?";
+            $query = $conectar->prepare($sql);
+            $query->bindValue(1,$idcliente);
+            $query->execute();
+            return $query->fetchAll(PDO::FETCH_ASSOC);
+        }        
+        /*TODO: Eliminar Cliente*/
+        public function eliminarCliente($idcliente){
+            $conectar = parent::Conexion();
+            $sql = "spEliminarCliente ?";
+            $query = $conectar->prepare($sql);
+            $query->bindValue(1,$idcliente);
+            $query->execute();
+        }        
+        /*TODO: Insertar Cliente*/
+        public function insertarCliente($idempresa, $nombrecliente, $ruccliente, $telefonocliente,$direccioncliente,$correocliente){
+            $conectar = parent::Conexion();
+            $sql = "spRegistrarCliente ?,?,?,?,?,?";
+            $query = $conectar->prepare($sql);
+            $query->bindValue(1,$idempresa);
+            $query->bindValue(2,$nombrecliente);
+            $query->bindValue(3,$ruccliente);
+            $query->bindValue(4,$telefonocliente);
+            $query->bindValue(5,$direccioncliente);
+            $query->bindValue(6,$correocliente);
+            $query->execute();
+            
+        }        
+        /*TODO:Actualizar Registro*/
+        public function updateCliente($idcliente, $idempresa, $nombrecliente, $ruccliente, $telefonocliente,$direccioncliente,$correocliente){
+            $conectar = parent::Conexion();
+            $sql = "spUpdateCliente ?,?,?";
+            $query = $conectar->prepare($sql);
+            $query->bindValue(1,$idcliente);
+            $query->bindValue(2,$idempresa);
+            $query->bindValue(3,$nombrecliente);
+            $query->bindValue(3,$ruccliente);
+            $query->bindValue(3,$telefonocliente);
+            $query->bindValue(3,$direccioncliente);
+            $query->bindValue(3,$correocliente);
+            $query->execute();
+        }        
+
+    }
+
+?>
